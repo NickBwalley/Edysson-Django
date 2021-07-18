bce1a20674b754fc5be39a28f3c6833ebf994630
NickBwalley <nickbwalley@gmail.com>
2021-07-18 16:57:39 +0300

    Edysson

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.home),
+	path('', views.home, name='home'),
+	path('register/', views.register_page, name='register'),
+	path('login/', views.login_page, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-	return render('snippets/index.html')
+	return render(request, 'accounts/index.html')
+	
+
+def register_page(request):
+	return render(request, 'accounts/register.html')
+
+
+def login_page(request):
+	return render(request, 'accounts/login.html')
 
 
